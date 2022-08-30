7433e4e89461d0be3a515a73fb01cfec17ba09c8
Awis Alqarni <awis0330@gmail.com>
2022-08-30 08:54:23 +0800

    fix: deleted duplicated

--- init-test.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    document.querySelectorAll('[data-bs-toggle="tooltip"]')
-      .forEach(tooltip => {
-        new bootstrap.Tooltip(tooltip)
-      });
-  }, 3000);
-  console.log("Window is loaded!");
-});
